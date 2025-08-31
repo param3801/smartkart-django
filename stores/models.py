219c6174150d0b0f08fa397a6d71bf1e10eeb6b8
param-99 <pappu.2327cseml1148@kiet.edu>
2025-08-31 12:39:40 +0530

    completed up to view products

--- stores/models.py
+++ stores/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 class Product(models.Model):
@@ -16,6 +17,11 @@ class Product(models.Model):
   class Meta:
     verbose_name = 'product'
     verbose_name_plural = 'products'
+
+  def get_url(self):
+    return f'/store/{self.category.slug}/{self.slug}/'
+    # return reverse('product_detail', args=[self.category.slug, self.slug])
+  
 
   def __str__(self):
     return self.product_name
