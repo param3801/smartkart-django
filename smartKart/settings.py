a29f67a74ec048ad2b00da43ecc9bc06a507bcf5
param-99 <pappu.2327cseml1148@kiet.edu>
2025-09-05 17:20:16 +0530

    up to cart function

--- smartKart/settings.py
+++ smartKart/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'category',
     'accounts',
     'stores',
+    'carts',
 ]
 
 MIDDLEWARE = [
--- smartKart/urls.py
+++ smartKart/urls.py
@@ -25,5 +25,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('store/', include('stores.urls')),
+    path('cart/', include('carts.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
