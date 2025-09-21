136558839dc88306678acb71e77a8060aa363696
param-99 <pappu.2327cseml1148@kiet.edu>
2025-09-21 19:23:10 +0530

    completed till payment

--- orders/urls.py
+++ orders/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from orders import views
 urlpatterns = [
     path('placeorder/',views.placeorder,name="placeorder"),
-    path('payment/',views.payment,name="payment")
+    path('payment/',views.payment,name="payment"),
+    path('order_complete/',views.order_complete,name="order_complete"),
+    
+
 
 ]
