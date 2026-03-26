611ea723d3a4f07aa7ec81fc83188e08b6bea78e
param-99 <pappu.2327cseml1148@kiet.edu>
2026-03-26 20:00:42 +0530

    add mysql

--- smartKart/settings.py
+++ smartKart/settings.py
@@ -89,10 +89,25 @@ AUTH_USER_MODEL = 'accounts.Account'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+# MySQL database configuration
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'smart_kart_database',
+        'USER': 'root',
+        'PASSWORD': 'Param1234@',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'OPTIONS': {
+            'charset': 'utf8mb4',
+        }
     }
 }
 
